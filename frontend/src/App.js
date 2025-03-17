5fba22ee2b888d0cae429836559197b31b95666d
anyabot <52899201+anyabot@users.noreply.github.com>
2025-03-17 14:11:00 +0700

    Implement Protected Routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,7 @@ import MemberCreate from './pages/Members/MemberCreate';
 import CourseList from './pages/Courses/CourseList';
 import CourseEdit from './pages/Courses/CourseEdit';
 import CourseCreate from './pages/Courses/CourseCreate';
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   return (
@@ -16,12 +17,12 @@ function App() {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/members/list" element={<MemberList />} />
-        <Route path="/members/edit/:id" element={<MemberEdit />} />
-        <Route path="/members/create" element={<MemberCreate />} />
-        <Route path="/courses/list" element={<CourseList />} />
-        <Route path="/courses/edit/:id" element={<CourseEdit />} />
-        <Route path="/courses/create" element={<CourseCreate />} />
+        <Route path="/members/list" element={<ProtectedRoute element={<MemberList />} />} />
+        <Route path="/members/edit/:id" element={<ProtectedRoute element={<MemberEdit />} />} />
+        <Route path="/members/create" element={<ProtectedRoute element={<MemberCreate />} />} />
+        <Route path="/courses/list" element={<ProtectedRoute element={<CourseList />} />} />
+        <Route path="/courses/edit/:id" element={<ProtectedRoute element={<CourseEdit />} />} />
+        <Route path="/courses/create" element={<ProtectedRoute element={<CourseCreate />} />} />
       </Routes>
     </Router>
   );

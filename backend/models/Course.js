ceb2a81e7a7efd98496b9951bf8b2a2fbd94df77
anyabot <52899201+anyabot@users.noreply.github.com>
2025-03-26 16:39:22 +0700

    Change Teacher to be not required

--- backend/models/Course.js
+++ backend/models/Course.js
@@ -4,7 +4,7 @@ const courseSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
   title: { type: String, required: true, },
   description: { type: String, },
-  teacher: { type: mongoose.Schema.Types.ObjectId, ref: "Member", },
+  teacher: { type: mongoose.Schema.Types.ObjectId, ref: "Member", required: false, default: null},
   startDate: { type: Date },
   endDate: { type: Date },
 });

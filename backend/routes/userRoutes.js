d0cf6cc0e013750afd6c4f78692b0847ac9e6917
anyabot <52899201+anyabot@users.noreply.github.com>
2025-05-05 21:12:33 +0700

    - Added Middleware for role-based protection

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,9 +2,10 @@
 const express = require('express');
 const { getUsers, addUser, getUser, updateUser, deleteUser } = require('../controllers/authController');
 const { protect } = require('../middleware/authMiddleware');
+const allowedRoles = require('../middleware/roleMiddleware');
 const router = express.Router();
 
-router.route('/').get(protect, getUsers).post(protect, addUser);
-router.route('/:id').get(protect, getUser).put(protect, updateUser).delete(protect, deleteUser);
+router.route('/').get(protect, getUsers).post(protect, allowedRoles('Admin'), addUser);
+router.route('/:id').get(protect, getUser).put(protect, allowedRoles('Admin'), updateUser).delete(protect, allowedRoles('Admin'), deleteUser);
 
 module.exports = router;

f67793a4cc85c53e2578233445a0d01d0280d831
anyabot <52899201+anyabot@users.noreply.github.com>
2025-05-12 17:00:14 +0700

    - Added course authorization

--- backend/routes/courseRoutes.js
+++ backend/routes/courseRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const { getCourses, addCourse, updateCourse, deleteCourse, getCourse } = require('../controllers/courseController');
 const { protect } = require('../middleware/authMiddleware');
+const allowedRoles = require('../middleware/roleMiddleware');
 const router = express.Router();
 
-router.route('/').get(protect, getCourses).post(protect, addCourse);
-router.route('/:id').get(protect, getCourse).put(protect, updateCourse).delete(protect, deleteCourse);
+router.route('/').get(protect, getCourses).post(protect, allowedRoles('Admin'), addCourse);
+router.route('/:id').get(protect, getCourse).put(protect, allowedRoles('Admin', 'Teacher'), updateCourse).delete(protect, allowedRoles('Admin'), deleteCourse);
 
 module.exports = router;

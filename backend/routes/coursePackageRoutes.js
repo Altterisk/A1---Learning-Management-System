ffafa55432afc395d02bb978085043ad2ad0b926
me-li-za <labastida.meliza@gmail.com>
2025-05-14 19:22:33 +1000

    restrict roles for creating a course package in routes

--- backend/routes/coursePackageRoutes.js
+++ backend/routes/coursePackageRoutes.js
@@ -1,8 +1,11 @@
 const express = require('express');
-const { createCoursePackage } = require('../controllers/coursepackageController');
+const { createCoursePackage, getCoursePackages } = require('../controllers/coursepackageController');
 const { protect } = require('../middleware/authMiddleware');
+const allowedRoles = require('../middleware/roleMiddleware');
 const router = express.Router();
 
-router.route('/').post(protect, createCoursePackage);
+router.route('/').post(protect, allowedRoles('Admin'), createCoursePackage);
+router.route('/').get(protect, allowedRoles('Admin'), getCoursePackages);
+//router.route('/').get(protect, getCoursePackages).post(protect, allowedRoles('Admin'), createCoursePackage);
 
 module.exports = router;

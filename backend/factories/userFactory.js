b3ea07a75f83ba920b3bc87f60c5fe3e027a5918
anyabot <52899201+anyabot@users.noreply.github.com>
2025-05-16 08:16:03 +0700

    - Added SIngleton to Factory

--- backend/factories/userFactory.js
+++ backend/factories/userFactory.js
@@ -3,7 +3,16 @@ const Teacher = require('../models/Teacher');
 const Admin = require('../models/Admin');
 
 class UserFactory {
-  static async createUser({ role, firstName, lastName, email, password, dateOfBirth, ...rest }) {
+  static instance;
+
+  constructor() {
+    if (UserFactory.instance) {
+      return UserFactory.instance;
+    }
+    UserFactory.instance = this;
+  }
+
+  async createUser({ role, firstName, lastName, email, password, dateOfBirth, ...rest }) {
     switch (role) {
       case 'Student':
         return await Student.create({ role, firstName, lastName, email, password, dateOfBirth, ...rest });
@@ -17,4 +26,7 @@ class UserFactory {
   }
 }
 
-module.exports = UserFactory;
+const factoryInstance = new UserFactory();
+Object.freeze(factoryInstance);
+
+module.exports = factoryInstance;
